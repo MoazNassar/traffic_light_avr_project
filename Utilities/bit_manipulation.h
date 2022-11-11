f0f1eb3a58ed392d6a30b98c4991f7e8b2cbbdba
MoazNassar <42101601+MoazNassar@users.noreply.github.com>
2022-11-11 20:25:57 +0200

    Add files via upload

---
+++ Utilities/bit_manipulation.h
@@ -0,0 +1,18 @@
+
+
+#ifndef BIT_MANIPULATION_H_
+#define BIT_MANIPULATION_H_
+
+
+#define SET_BIT(REG, BIT) (REG|=(1<<BIT))
+#define CLR_BIT(REG, BIT) (REG&=(~(1<<BIT)))
+#define GET_BIT(REG, BIT) (1&(REG>>BIT))
+#define TOG_BIT(REG, BIT) (REG^=(1<<BIT))
+#define WRT_BIT(REG, BIT, VAL) ((REG = REG&~(1<<BIT)|(VAL<<BIT)))
+
+
+
+
+
+
+#endif /* BIT_MANIPULATION_H_ */
